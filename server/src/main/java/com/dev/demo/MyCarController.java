e03e920e69b8752267245adda627ffac6d0eaa86
Paras Gautam <parasgautam2011@gmail.com>
2018-12-02 16:19:50 -0600

    added CORS

--- server/src/main/java/com/dev/demo/MyCarController.java
+++ server/src/main/java/com/dev/demo/MyCarController.java
@@ -3,6 +3,7 @@ package com.dev.demo;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class MyCarController {
 	}
 	
 	@GetMapping("/cool-cars")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Collection<Car> getMyCoolCars(){
 		return carRepository.findAll().stream().filter(this::isCool).collect(Collectors.toList());
 	}
